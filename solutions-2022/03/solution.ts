221102e3d79ee993933a76ba98428826730eb6de
TomM <mc_vibraphone@proton.me>
2022-12-03 18:12:34 -0500

    2 star answer - need to get a better handle on how types are propagated through the JS runtime.

--- solutions-2022/03/solution.ts
+++ solutions-2022/03/solution.ts
@@ -2,45 +2,43 @@
 // https://adventofcode.com/2022/day/3
 // https://adventofcode.com/2022/day/3/input
 
-//import * as stream from 'stream';
+import * as stream from 'stream';
 import * as fs from 'fs';
 import * as readline from 'readline';
 
-const testData = [
-  'vJrwpWtwJgWrhcsFMMfFFhFp',
-  'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
-  'PmmdzqPrVvPwwTWBwg',
-  'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
-  'ttgJtRGJQctTZtZT',
-  'CrZsJsPPZsGzwwsLwLmpwMDw',
+const testData:string[] = [
+	'vJrwpWtwJgWrhcsFMMfFFhFp',
+	'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+	'PmmdzqPrVvPwwTWBwg',
+	'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+	'ttgJtRGJQctTZtZT',
+	'CrZsJsPPZsGzwwsLwLmpwMDw',
 ]
 
-export const rucksackReorganization = () => {
+export const badgeLocators = () => {
 
-	//const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
+//	const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
 
 	const rl = readline.createInterface(fs.createReadStream(`${__dirname}/data.txt`));
 
-	let all_dupes = [];
+	let all_badges = [];
+
+	let group_ctr = 0;
+
+	let sets = [];
 
 	rl.on('line', (line:string) => {
-		let halfway = Math.trunc(line.length / 2);
-
-		if((halfway * 2) != line.length) {
-			console.error(`Line :: ${line} :: is not an even length.`);
-			return false;
-		}
-
-		let set_0 = new Set(line.slice(0, halfway).trim().split(""));
-		let set_1 = new Set(line.slice(halfway).trim().split(""));
-
-		let intersection = [...set_0].filter( item => set_1.has(item));
-		if(intersection.length != 1)
+		console.log(line);
+		if(sets.length < 3)
 		{
-			console.error(`bad intersection in ${line}`);
-			return false;
+			sets.push(new Set([...line]));
 		}
-		intersection.forEach((item) => all_dupes.push(item));
+		if(sets.length >= 3) {
+			sets.forEach(set => { console.log(`set :: ${[...set]}`); });
+			all_badges.push([...(sets[0])].filter(item => sets[1].has(item)).filter(item => sets[2].has(item)));
+			group_ctr = 0;
+			sets = [];
+		}
 
 	});
 
@@ -50,16 +48,19 @@ export const rucksackReorganization = () => {
 	// and 'A' into 27 through 'Z' into 52.
 	// Jerks. :P
 
-	const convert_char_value = (char:string) => (char[0] > 'Z')
-		? (char.charCodeAt(0) - 96)
-		: (char.charCodeAt(0) - 38);
+	const convert_char_value = (char:string) => (char.toString()[0] > 'Z')
+		? char.toString().charCodeAt(0) - 96
+		: char.toString().charCodeAt(0) - 38;
 
 	rl.on('close', () => {
+
 		console.log(`EOF`);
-		let priorities_sum = [...all_dupes].reduce(
+		console.log(`Badge list is ${all_badges}`);
+
+		let this_sum = all_badges.reduce(
 			(total:number, item:string) => total + convert_char_value(item), 0);
 
-		console.log(`Summed priorities is ${priorities_sum}`);
+		console.log(`Summed priorities is ${this_sum}`);
 	});
 
 	rl.on('error', (error:Error) => {
