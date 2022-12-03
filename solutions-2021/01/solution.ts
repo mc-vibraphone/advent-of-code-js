e90ad438fca582e50b4c54d59875cb0100bcb241
Joe Davis <jdavis@grayshift.com>
2022-12-02 22:57:54 -0600

    loading past solutions

---
+++ solutions-2021/01/solution.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from 'fs'
+
+// https://adventofcode.com/2021/day/1
+// https://adventofcode.com/2021/day/1/input
+
+const testData = [
+  '199',
+  '200',
+  '208',
+  '210',
+  '200',
+  '207',
+  '240',
+  '269',
+  '260',
+  '263',
+]
+
+export const sonarSweep = () => {
+  const data = readFileSync(`${__dirname}/data.txt`, 'utf8')
+    .split('\n')
+    .map(i => parseInt(i))
+  let increasedMeasurements1 = 0
+  data.forEach((measurement, idx) => {
+    if (idx > 0 && measurement > data[idx - 1]) {
+      increasedMeasurements1++
+    }
+  })
+
+  let increasedMeasurements2 = 0
+  data.forEach((_, idx) => {
+    if (
+      idx > 0 &&
+      data.slice(idx, idx + 3).reduce((sum, val) => sum + val) >
+        data.slice(idx - 1, idx + 2).reduce((sum, val) => sum + val) &&
+      data.slice(idx, idx + 3).length === 3
+    ) {
+      increasedMeasurements2++
+    }
+  })
+
+  return [increasedMeasurements1, increasedMeasurements2]
+}
---
+++ solutions-2021/02/solution.ts
@@ -0,0 +1,53 @@
+import { readFileSync } from 'fs'
+
+// https://adventofcode.com/2021/day/2
+// https://adventofcode.com/2021/day/2/input
+
+const testData = [
+  'forward 5',
+  'down 5',
+  'forward 8',
+  'up 3',
+  'down 8',
+  'forward 2',
+]
+
+export const dive = () => {
+  const data = readFileSync(`${__dirname}/data.txt`, 'utf8').split('\n')
+
+  const position1 = data.reduce(
+    (position, move) => {
+      const [direction, magnitude] = move.split(' ')
+      if (direction === 'forward') {
+        position.distance += parseInt(magnitude)
+      } else if (direction === 'down') {
+        position.depth += parseInt(magnitude)
+      } else if (direction === 'up') {
+        position.depth -= parseInt(magnitude)
+      }
+      return position
+    },
+    { distance: 0, depth: 0 },
+  )
+
+  const position2 = data.reduce(
+    (position, move) => {
+      const [direction, magnitude] = move.split(' ')
+      if (direction === 'forward') {
+        position.distance += parseInt(magnitude)
+        position.depth += position.aim * parseInt(magnitude)
+      } else if (direction === 'down') {
+        position.aim += parseInt(magnitude)
+      } else if (direction === 'up') {
+        position.aim -= parseInt(magnitude)
+      }
+      return position
+    },
+    { distance: 0, depth: 0, aim: 0 },
+  )
+
+  return [
+    position1.distance * position1.depth,
+    position2.distance * position2.depth,
+  ]
+}
