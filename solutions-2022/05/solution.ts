1ace5bb647f8fee0348a2c2c1f9bfdfd12adf1e1
TomM <mc_vibraphone@proton.me>
2022-12-09 15:23:37 -0500

    wip

--- solutions-2022/05/solution.ts
+++ solutions-2022/05/solution.ts
@@ -1,7 +1,11 @@
-import { readFileSync } from 'fs'
 
 // https://adventofcode.com/2022/day/5
 // https://adventofcode.com/2022/day/5/input
+
+import * as stream from 'stream';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
 
 const testData = [
   '    [D]    ',
@@ -15,8 +19,33 @@ const testData = [
   'move 1 from 1 to 2',
 ]
 
+	const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
+
+	//const rl = readline.createInterface(fs.createReadStream(`${__dirname}/data.txt`));
+
 export const supplyStacks = () => {
-  const data = readFileSync(`${__dirname}/data.txt`, 'utf8').split('\n')
-  console.log(testData)
+
+  let stacks:string[] = [];
+  let lines:string[] = [];
+
+  rl.on('line', (line: string) => {
+    if(!line.includes('1')) {
+      lines.push(line);
+    }
+    else{
+      lines.forEach(line => { console.log(line); });
+      console.log(line);
+    }
+  });
+  
+  rl.on('close', () => {
+
+    console.log(`EOF`);
+    
+  });
+
+  rl.on('error', (error: Error) => {
+    console.log(error);
+  });
   return 0
 }
