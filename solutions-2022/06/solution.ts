c8b442bc72f8fa04e25d961a8f88109aa8fabd0f
TomM <mc_vibraphone@proton.me>
2022-12-09 22:42:32 -0500

    one star complete go go Set() power.

--- solutions-2022/06/solution.ts
+++ solutions-2022/06/solution.ts
@@ -2,13 +2,50 @@ import { readFileSync } from 'fs'
 
 // https://adventofcode.com/2022/day/6
 // https://adventofcode.com/2022/day/6/input
+//
+import * as stream from 'stream';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
 
 const testData = [
-  'mjqjpqmgbljsphdztnvjfqwrcgsml',
+
+	'mjqjpqmgbljsphdztnvjfqwrcgsml',
+	'bvwbjplbgvbhsrlpgdmjqwftvncz',
+	'nppdvjthqldpwncqszvftbrmjlhg',
+	'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',
+	'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
 ]
 
 export const tuningTrouble = () => {
-  const data = readFileSync(`${__dirname}/data.txt`, 'utf8').split('\n')
-  console.log(testData)
-  return 0
+
+	//const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
+
+	const rl = readline.createInterface(fs.createReadStream(`${__dirname}/data.txt`));
+
+	rl.on('line', (line:string) => {
+		console.log(line);
+		let _data = [...line];
+		let _start = 0;
+		let _end = 4;
+		let buff = new Set();
+		_data.slice(_start, _end).forEach(x => buff.add(x));
+		while(buff.size != 4 && _end < _data.length) {
+			buff = new Set();
+			_start++, _end++;
+			_data.slice(_start, _end).forEach(x => buff.add(x));
+		}
+		if(4 == buff.size) {
+			console.log(`EOM is at ${_end}`);
+		}
+		else {
+			console.log(`No marker found.`);
+		}
+	});
+
+	rl.on('end', () => {
+		console.log("EOF");
+	});
+
+	return 0
 }
