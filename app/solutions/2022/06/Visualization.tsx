333c861a01b681bb5f0b2b468bb58e8f50bf6877
Joe Davis <jdavis@grayshift.com>
2022-12-19 21:01:24 -0600

    2022-06

---
+++ app/solutions/2022/06/Visualization.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import * as solutionExports from './solution'
+import type { FC } from 'react'
+import type { VisualizationComponentInfo } from '~/lib/types/VisualizationComponentInfo'
+
+interface TuningTroubleVisualizationProps {
+  componentInfo: VisualizationComponentInfo
+}
+
+const TuningTroubleVisualization: FC<TuningTroubleVisualizationProps> = ({
+  componentInfo,
+}) => {
+  const { testData, puzzleData } = solutionExports['solutionData']
+  const data = testData
+
+  useEffect(() => {
+    console.log(componentInfo)
+    console.log(Object.keys(solutionExports))
+    console.log(testData, puzzleData)
+  }, [])
+  return (
+    <div className="h-full">
+      <div> Tuning Trouble Visualization</div>
+    </div>
+  )
+}
+
+export default TuningTroubleVisualization
---
+++ app/solutions/2022/06/solution.ts
@@ -0,0 +1,41 @@
+// https://adventofcode.com/2022/day/6
+// https://adventofcode.com/2022/day/6/input
+
+import { uniq } from 'lodash'
+import { puzzleData, testData } from './data'
+
+const part1 = (data: string) => {
+  let result = 0
+  for (let i = 4; i <= data.length; i++) {
+    if (uniq(data.slice(i - 4, i)).length === 4) {
+      result = i
+      break
+    }
+  }
+  return result
+}
+
+const part2 = (data: string) => {
+  let result = 0
+  for (let i = 14; i <= data.length; i++) {
+    if (uniq(data.slice(i - 14, i)).length === 14) {
+      result = i
+      break
+    }
+  }
+  return result
+}
+
+export const solutionData = {
+  puzzleData,
+  testData,
+}
+
+export const tuningTrouble = () => {
+  const useTestData = false
+  const data = useTestData ? testData : puzzleData
+
+  return [part1(data[0]), part2(data[0])]
+}
+
+// Other exports should go below
