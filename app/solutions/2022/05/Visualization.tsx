4b213de450828c79355a0d5f48d991d2acec247c
Joe Davis <jdavis@grayshift.com>
2022-12-19 19:59:46 -0600

    2022-05

---
+++ app/solutions/2022/05/Visualization.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import * as solutionExports from './solution'
+import type { FC } from 'react'
+import type { VisualizationComponentInfo } from '~/lib/types/VisualizationComponentInfo'
+
+interface SupplyStacksVisualizationProps {
+  componentInfo: VisualizationComponentInfo
+}
+
+const SupplyStacksVisualization: FC<SupplyStacksVisualizationProps> = ({
+  componentInfo,
+}) => {
+  const { testData, puzzleData } = solutionExports['solutionData']
+  const data = testData
+
+  useEffect(() => {
+    console.log(componentInfo)
+    console.log(Object.keys(solutionExports))
+    console.log(testData, puzzleData)
+  }, [])
+  return (
+    <div className="h-full">
+      <div> Supply Stacks Visualization</div>
+    </div>
+  )
+}
+
+export default SupplyStacksVisualization
---
+++ app/solutions/2022/05/solution.ts
@@ -0,0 +1,70 @@
+// https://adventofcode.com/2022/day/5
+// https://adventofcode.com/2022/day/5/input
+
+import { puzzleData, testData } from './data'
+
+const part1 = (data: string[]) => {
+  const dataSeparator = data.indexOf('')
+  const stacks = composeStacks(data)
+  for (let i = dataSeparator + 1; i < data.length; i++) {
+    const manuever = data[i]
+      .replace('move ', '')
+      .replace('from ', '')
+      .replace('to ', '')
+      .split(' ')
+      .map(_ => parseInt(_))
+    for (let n = 1; n <= manuever[0]; n++) {
+      const block = stacks[manuever[1] - 1].pop() as string
+      stacks[manuever[2] - 1].push(block)
+    }
+  }
+  return stacks.map(s => s[s.length - 1]).join('')
+}
+
+const part2 = (data: string[]) => {
+  const dataSeparator = data.indexOf('')
+  const stacks = composeStacks(data)
+  for (let i = dataSeparator + 1; i < data.length; i++) {
+    const manuever = data[i]
+      .replace('move ', '')
+      .replace('from ', '')
+      .replace('to ', '')
+      .split(' ')
+      .map(_ => parseInt(_))
+
+    let selection = stacks[manuever[1] - 1].splice(
+      stacks[manuever[1] - 1].length - manuever[0],
+      manuever[0],
+    )
+    stacks[manuever[2] - 1].push(...selection)
+  }
+  return stacks.map(s => s[s.length - 1]).join('')
+}
+
+export const solutionData = {
+  puzzleData,
+  testData,
+}
+
+const composeStacks = (data: string[]) => {
+  const dataSeparator = data.indexOf('')
+  const stacks: string[][] = data[dataSeparator - 2].split(' ').map(_ => [])
+
+  for (let i = dataSeparator - 2; i >= 0; i--) {
+    for (let s = 0; s < stacks.length; s++) {
+      if (data[i].slice(s * 4 + 1, s * 4 + 2) !== ' ') {
+        stacks[s].push(data[i].slice(s * 4 + 1, s * 4 + 2))
+      }
+    }
+  }
+  return stacks
+}
+
+export const supplyStacks = () => {
+  const useTestData = false
+  const data = useTestData ? testData : puzzleData
+
+  return [part1(data), part2(data)]
+}
+
+// Other exports should go below
