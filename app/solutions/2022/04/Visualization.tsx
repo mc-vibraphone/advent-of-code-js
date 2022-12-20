451bcc35f0f0b63b3e49174f0ea63cee993aff48
Joe Davis <jdavis@grayshift.com>
2022-12-19 18:11:48 -0600

    2022-04

---
+++ app/solutions/2022/04/Visualization.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import * as solutionExports from './solution'
+import type { FC } from 'react'
+import type { VisualizationComponentInfo } from '~/lib/types/VisualizationComponentInfo'
+
+interface CampCleanupVisualizationProps {
+  componentInfo: VisualizationComponentInfo
+}
+
+const CampCleanupVisualization: FC<CampCleanupVisualizationProps> = ({
+  componentInfo,
+}) => {
+  const { testData, puzzleData } = solutionExports['solutionData']
+  const data = testData
+
+  useEffect(() => {
+    console.log(componentInfo)
+    console.log(Object.keys(solutionExports))
+    console.log(testData, puzzleData)
+  }, [])
+  return (
+    <div className="h-full">
+      <div> Camp Cleanup Visualization</div>
+    </div>
+  )
+}
+
+export default CampCleanupVisualization
