c09b5c29e435c4d1dc86eebe66568e404a9bfba3
Joe Davis <jdavis@grayshift.com>
2023-11-06 17:47:52 -0600

    solution for 2015 13

---
+++ app/lib/util.ts
@@ -0,0 +1,21 @@
+const permutator = (inputArr: any[]) => {
+  let result: any[] = []
+
+  const permute = (arr: any[], m: any[] = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice()
+        let next = curr.splice(i, 1)
+        permute(curr.slice(), m.concat(next))
+      }
+    }
+  }
+
+  permute(inputArr)
+
+  return result
+}
+
+export { permutator }
