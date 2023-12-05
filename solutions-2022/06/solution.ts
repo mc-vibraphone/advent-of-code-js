8f3d18bc54a346a987a5be785d296e446fff4f0a
Tom[1] <tmcmanus@grayshift.com>
2023-12-04 19:26:06 -0500

    one star complete go go Set() power.

--- solutions-2022/06/solution.ts
+++ solutions-2022/06/solution.ts
@@ -3,11 +3,15 @@ import { readFileSync } from 'fs'
 // https://adventofcode.com/2022/day/6
 // https://adventofcode.com/2022/day/6/input
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> cd2496a (one star complete go go Set() power.)
 //
 import * as stream from 'stream';
 import * as fs from 'fs';
 import * as readline from 'readline';
 
+<<<<<<< HEAD
 
 const testData = [
 
@@ -56,14 +60,54 @@ export const tuningTrouble = () => {
 
 	return 0
 =======
+=======
+>>>>>>> cd2496a (one star complete go go Set() power.)
 
 const testData = [
-  'mjqjpqmgbljsphdztnvjfqwrcgsml',
+
+	'mjqjpqmgbljsphdztnvjfqwrcgsml',
+	'bvwbjplbgvbhsrlpgdmjqwftvncz',
+	'nppdvjthqldpwncqszvftbrmjlhg',
+	'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',
+	'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
 ]
 
 export const tuningTrouble = () => {
+<<<<<<< HEAD
   const data = readFileSync(`${__dirname}/data.txt`, 'utf8').split('\n')
   console.log(testData)
   return 0
 >>>>>>> 1ea1d7f (day 6 initial commit)
+=======
+
+	//const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
+
+	const rl = readline.createInterface(fs.createReadStream(`${__dirname}/data.txt`));
+
+	rl.on('line', (line:string) => {
+		console.log(line);
+		let _data = [...line];
+		let _start = 0;
+		let _end = 4;
+		let buff = new Set();
+		_data.slice(_start, _end).forEach(x => buff.add(x));
+		while(buff.size != 4 && _end < _data.length) {
+			buff = new Set();
+			_start++, _end++;
+			_data.slice(_start, _end).forEach(x => buff.add(x));
+		}
+		if(4 == buff.size) {
+			console.log(`EOM is at ${_end}`);
+		}
+		else {
+			console.log(`No marker found.`);
+		}
+	});
+
+	rl.on('end', () => {
+		console.log("EOF");
+	});
+
+	return 0
+>>>>>>> cd2496a (one star complete go go Set() power.)
 }
