01a499c4e68648aeed2f466e523023aa974d379b
Tom[1] <tmcmanus@grayshift.com>
2023-12-04 19:21:22 -0500

    :w Worked through part one after misinterpreting the reqs.

--- solutions-2022/03/solution.ts
+++ solutions-2022/03/solution.ts
@@ -2,6 +2,7 @@
 // https://adventofcode.com/2022/day/3
 // https://adventofcode.com/2022/day/3/input
 
+<<<<<<< HEAD
 import * as stream from 'stream';
 <<<<<<< HEAD
 import * as fs from 'fs';
@@ -40,6 +41,11 @@ export const badgeLocators = () => {
 			group_ctr = 0;
 			sets = [];
 =======
+=======
+//import * as stream from 'stream';
+import * as fs from 'fs';
+import * as readline from 'readline';
+>>>>>>> 2c5108a (Worked through part one after misinterpreting the reqs.)
 
 const testData = [
   'vJrwpWtwJgWrhcsFMMfFFhFp',
@@ -51,33 +57,46 @@ const testData = [
 ]
 
 export const rucksackReorganization = () => {
-	//const fs = require('fs');
 
-	const readline = require('node:readline');
+	//const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
 
-	const rl = readline.createInterface(stream.Readable.from(testData.join(('\n'))));
+	const rl = readline.createInterface(fs.createReadStream(`${__dirname}/data.txt`));
 
-	//const rl = readline.createInterface(fs.createReadStream(`${__dirname}/data.txt`));
-
-	let priority_sum = 0;
+	let all_dupes = [];
 
 	rl.on('line', (line:string) => {
-		console.log(line);
-		if(0 != (line.length % 2)) {
-			console.error(`Line :: ${line} :: is not an even lengthi.`);
+		let halfway = Math.trunc(line.length / 2);
+
+		if((halfway * 2) != line.length) {
+			console.error(`Line :: ${line} :: is not an even length.`);
 			return false;
 >>>>>>> 375a80e (Initial day 1 2 3 checkin)
 		}
 
+		let set_0 = new Set(line.slice(0, halfway).trim().split(""));
+		let set_1 = new Set(line.slice(halfway).trim().split(""));
+
+		let intersection = [...set_0].filter( item => set_1.has(item));
+		if(intersection.length != 1)
+		{
+			console.error(`bad intersection in ${line}`);
+			return false;
+		}
+		intersection.forEach((item) => all_dupes.push(item));
+
 	});
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 2c5108a (Worked through part one after misinterpreting the reqs.)
 	// They have annoyingly flipped lower and uppercase values from the axiomatically
 	// correct ASCII values.
 	// This turnes 'a' into 1 then through 'z' to 26
 	// and 'A' into 27 through 'Z' into 52.
 	// Jerks. :P
 
+<<<<<<< HEAD
 	const convert_char_value = (char:string) => (char.toString()[0] > 'Z')
 		? char.toString().charCodeAt(0) - 96
 		: char.toString().charCodeAt(0) - 38;
@@ -96,6 +115,18 @@ export const rucksackReorganization = () => {
 		console.log(`EOF`);
 		console.log(`Priority sum was ${priority_sum}`);
 >>>>>>> 375a80e (Initial day 1 2 3 checkin)
+=======
+	const convert_char_value = (char:string) => (char[0] > 'Z')
+		? (char.charCodeAt(0) - 96)
+		: (char.charCodeAt(0) - 38);
+
+	rl.on('close', () => {
+		console.log(`EOF`);
+		let priorities_sum = [...all_dupes].reduce(
+			(total:number, item:string) => total + convert_char_value(item), 0);
+
+		console.log(`Summed priorities is ${priorities_sum}`);
+>>>>>>> 2c5108a (Worked through part one after misinterpreting the reqs.)
 	});
 
 	rl.on('error', (error:Error) => {
