dd6c7107ff0d4b9c68c13da09ae376940e3f3ed2
Tom <tom.mcmanus@gmail.com>
2023-12-09 02:57:10 -0500

    Fix matchall consuming last token incorrectly with a lookahead group.

--- app/my-solutions/2023/01/solution.ts
+++ app/my-solutions/2023/01/solution.ts
@@ -11,12 +11,15 @@ const part2_test_data = [
 	'4nineeightseven2',
 	'zoneight234',
 	'7pqrstsixteen',
+	'one71ninesix8oneighthlx'
 ];
 
 
 const trebuchet = () => {
   const useTestData = false;
   const data = useTestData ? testData : puzzleData
+
+  console.assert(1000 == puzzleData.length)
 
   return [part1(data), part2(data)]
 }
@@ -51,14 +54,13 @@ const token_to_number = (token: string) => {
 			return 8;
 		case "nine":
 			return 9;
-		default:
-			console.assert(false);
-			return null;
 	}
+	console.assert(false, "Bad token");
+	return null;
 };
 
 const make_a_number = (tens_place: string, ones_place: string) => {
-	// See if we have a numeric character (so like 1 - 9) or a string token of a numer (so like one two ... eight nine)
+	// See if we have a numeric character (so like 1 - 9) or a string token of a number (so like one two ... eight nine)
 	let numeric_tens = parseInt(tens_place);
 	if(isNaN(numeric_tens)) {
 		numeric_tens = token_to_number(tens_place)!;
@@ -67,17 +69,25 @@ const make_a_number = (tens_place: string, ones_place: string) => {
 	if(isNaN(numeric_ones)) {
 		numeric_ones = token_to_number(ones_place)!;
 	}
+	console.assert(numeric_tens > 0);
 	console.assert(numeric_tens < 10);
+	console.assert(numeric_ones > 0);
 	console.assert(numeric_ones < 10);
-	console.assert(10 * numeric_tens + numeric_ones < 100);
+	console.assert(((10 * numeric_tens) + numeric_ones) > 0);
+	console.assert(((10 * numeric_tens) + numeric_ones) < 100);
 	return (10 * numeric_tens) + numeric_ones;
 };
+
 const part2 = (data: string[]) => {
 	let sum = 0;
+	let count = 0;
 	data.forEach((item) => {
-		const re = /\d|one|two|three|four|five|six|seven|eight|nine/g;
-		const values = [...item.matchAll(re)];
-		sum += make_a_number(values[0].toString(), values[values.length - 1].toString());
+		// We have to use a lookahead group to make sure we don't consume end tokens incorrectly - 
+		// for example `oneeight` would be regex'd to just `one` and not [`one`, `eight`]
+		const re = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g;
+		const values = Array.from(item.matchAll(re));
+		sum += make_a_number(values[0][1], values[values.length - 1][1].toString());
+		count++;
 	});
 	return sum;
 }
